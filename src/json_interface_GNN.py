2c79da0b19fa6d8e8b1490b8310bfcfc5c7d0623
JerroldHann <jerroldhann@outlook.com>
2025-10-09 19:29:34 +0800

    update: add an interface between input json data stream and GNN model

---
+++ src/json_interface_GNN.py
@@ -0,0 +1,16 @@
+from gnn_drive_inference import json_processing
+import json
+
+# just follow this sample
+test_json = json.dumps({
+    "step": 710,
+    "orig_id": "88640",
+    "dest_id": "60",
+    "amount": 1000.0,
+    "orig_old_balance": 10000.0,
+    "orig_new_balance": 9000.0,
+    "dest_old_balance": 4000.0,
+    "dest_new_balance": 5000.0
+})
+
+result_df = json_processing(test_json)
